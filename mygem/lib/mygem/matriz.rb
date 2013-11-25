7f095d694cf7196ab3c4782468274ac6097257c3
Karen <alu0100402001@ull.edu.es>
2013-11-25 11:05:12 +0000

    Utilizando metodo time para implementar la multiplicacion de matrices

--- mygem/lib/mygem/matriz.rb
+++ mygem/lib/mygem/matriz.rb
@@ -54,15 +54,15 @@ class Matriz
     end
   end
   
-  def +(other)
+  def +(other)#con metodo times
     raise TypeError, 'No se pueden sumar Matrices de distintas dimensiones' unless (@filas==other.filas && @columnas == other.columnas)
 
     valor = Array.new
-
-    for i in 0...@filas
+    @filas.times do |i|
+    #for i in 0...@filas
       valor[i] = Array.new
-
-      for j in 0...@columnas
+       @columnas.times do |j|
+       #for j in 0...@columnas
         valor[i][j] = (self[i,j] + other[i,j])
       end
     end
@@ -90,14 +90,14 @@ class Matriz
     raise TypeError, 'No se pueden Multiplicar' unless (@columnas==other.filas)
 
     valor = Array.new
-
-    for i in 0...@filas
+    @filas.times do |i|
+    #for i in 0...@filas
       valor[i] = Array.new
-
-      for j in 0...other.columnas
+      @columnas.times do |j|
+      #for j in 0...other.columnas
         valor[i][j] = 0
-
-        for k in 0...@columnas
+        @columnas.times do |k|
+        #for k in 0...@columnas
           valor[i][j] += (self[i,k] * other[k,j])
         end
       end
