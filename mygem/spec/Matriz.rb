a02c33bcfcc326a5603d01914c83fb30706477f9
alu0100402001 <alu0100402001@ull.edu.es>
2013-11-29 09:20:59 +0000

    actualizando Matriz.rb

--- mygem/spec/Matriz.rb
+++ mygem/spec/Matriz.rb
@@ -1,10 +1,10 @@
 require "./lib/prct09.rb"
 
-describe Prct09::Matriz_Densa do
+describe Mygem::Matriz_Densa do
 
     before :each do
-        @m1 = Prct09::Matriz_Densa.new(2,2)
-        @m2 = Prct09::Matriz_Densa.new(2,2)
+        @m1 = Mygem::Matriz_Densa.new(2,2)
+        @m2 = Mygem::Matriz_Densa.new(2,2)
 
         @m1.set(0,0,1)
         @m1.set(0,1,2)
@@ -34,7 +34,7 @@ describe Prct09::Matriz_Densa do
 
     describe " # Operaciones con matrices densas. " do
         it " # Debe poderse sumar dos matrices " do
-                @m3 = Prct09::Matriz_Densa.new(2,2)
+                @m3 = Mygem::Matriz_Densa.new(2,2)
                 @m3.set(0,0,2)
                 @m3.set(0,1,4)
                 @m3.set(1,0,6)
@@ -44,7 +44,7 @@ describe Prct09::Matriz_Densa do
         end
 
         it " # Debe poderse restar dos matrices " do
-                @m3 = Prct09::Matriz_Densa.new(2,2)
+                @m3 = Mygem::Matriz_Densa.new(2,2)
                 @m3.set(0,0,0)
                 @m3.set(0,1,0)
                 @m3.set(1,0,0)
@@ -53,7 +53,7 @@ describe Prct09::Matriz_Densa do
                 (@m2-@m1).to_s.should eq(@m3.to_s)
         end
         it " # Debe poderse multiplicar dos matrices " do
-                @m3 = Prct09::Matriz_Densa.new(2,2)
+                @m3 = MYgem::Matriz_Densa.new(2,2)
                 @m3.set(0,0,7)
                 @m3.set(0,1,10)
                 @m3.set(1,0,15)
@@ -72,10 +72,10 @@ describe Prct09::Matriz_Densa do
     end
 end
 
-describe Prct09::Matriz_Dispersa do
+describe Mygem::Matriz_Dispersa do
 
     before :each do
-        @m1 = Prct09::Matriz_Densa.new(3,2)
+        @m1 = Mygem::Matriz_Densa.new(3,2)
 
         @m1.set(0,0,0)
         @m1.set(0,1,0)
@@ -86,15 +86,15 @@ describe Prct09::Matriz_Dispersa do
         @m1.set(2,0,0)
         @m1.set(2,1,0)
 
-        @md1 = Prct09::Matriz_Dispersa.copy(@m1)
-        @md2 = Prct09::Matriz_Dispersa.new(3,2)
+        @md1 = Mygem::Matriz_Dispersa.copy(@m1)
+        @md2 = Mygem::Matriz_Dispersa.new(3,2)
     end
 
     describe " # Almacenamiento de matrices. " do
 
         it " # Debe poderse crear matrices dispersas vacias o a partir de matrices densas." do
-            Prct09::Matriz_Dispersa.new(5, 5)
-            Prct09::Matriz_Dispersa.copy(@m1)
+            Mygem::Matriz_Dispersa.new(5, 5)
+            Mygem::Matriz_Dispersa.copy(@m1)
         end
 
         it " # Debe poderse acceder a los elementos de la matriz dispersa." do
@@ -119,7 +119,7 @@ describe Prct09::Matriz_Dispersa do
 
     describe " # Operaciones con matrices dispersas. " do
         it " # Debe poderse sumar dos matrices " do
-                @md3 = Prct09::Matriz_Dispersa.new(3,2)
+                @md3 = Mygem::Matriz_Dispersa.new(3,2)
 
                 @md3.set(0,0,0)
                 @md3.set(0,1,0)
@@ -133,7 +133,7 @@ describe Prct09::Matriz_Dispersa do
                 (@md1+@md2).to_s.should eq(@md3.to_s)
         end
         it " # Debe poderse restar dos matrices " do
-                @md3 = Prct09::Matriz_Dispersa.new(3,2)
+                @md3 = Mygem::Matriz_Dispersa.new(3,2)
                 @md3.set(0,0,0)
                 @md3.set(0,1,0)
 
@@ -146,12 +146,12 @@ describe Prct09::Matriz_Dispersa do
                 (@md2-@md1).to_s.should eq(@md3.to_s)
         end
         it " # Debe poderse multiplicar dos matrices " do
-                @md4 = Prct09::Matriz_Dispersa.new(2,3)
+                @md4 = Mygem::Matriz_Dispersa.new(2,3)
                 @md4.set(1,0,-1)
                 @md4.set(1,1,-1)
                 @md4.set(1,2,0)
 
-                @md3 = Prct09::Matriz_Dispersa.new(3,3)
+                @md3 = Mygem::Matriz_Dispersa.new(3,3)
                 @md3.set(0,0,0)
                 @md3.set(0,1,0)
                 @md3.set(0,2,0)
