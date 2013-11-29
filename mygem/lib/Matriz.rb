040dcce96fa5f977f227861b8dba1761f4d860d2
alu0100402001 <alu0100402001@ull.edu.es>
2013-11-29 08:47:58 +0000

    actualizando Matriz.rb

--- mygem/lib/Matriz.rb
+++ mygem/lib/Matriz.rb
@@ -1,10 +1,10 @@
-require "./lib/prct09/version"
-require "./lib/prct09/matriz.rb"
-require "./lib/prct09/matriz_densa.rb"
-require "./lib/prct09/matriz_dispersa.rb"
+require "./lib/mygem/version.rb"
+require "./lib/mygem/matriz.rb"
+require "./lib/mygem/matriz_densa.rb"
+require "./lib/mygem/matriz_dispersa.rb"
 
-require "./lib/prct09/racional.rb"
-require "./lib/prct09/gcd.rb"
+require "./lib/mygem/fracciones.rb"
+require "./lib/mygem/gcd.rb"
 
 module Prct09
   # Your code goes here..
