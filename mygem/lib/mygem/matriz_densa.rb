56be3044d3c7f772200ab9b756bb62cf30e5cee3
alu0100402001 <alu0100402001@ull.edu.es>
2013-11-29 08:57:56 +0000

    Utilizando metodo time para implementar operaciones con matrices

--- mygem/lib/mygem/matriz_densa.rb
+++ mygem/lib/mygem/matriz_densa.rb
@@ -12,42 +12,190 @@
 #
 # * metodo initialize
 # * metodo ==(other)
-# * metodo [](fila, columna)
-# * metodo []=(fila, columna, valor)
+# * metodo get
+# * metodo set
+# * metodo num_nulos
+# * metodo -(other)
+# * metodo *(other)
+# * metodo +(other)
+# * metodo to_s
+# * metodo max
+# * metodo min
+#
 #
 #
 
-class MatrizDensa < Matriz
-  attr_reader :filas, :columnas, :matriz
-  
-  def initialize(m)
-    super (m)
-  end
-    
-  def ==(other)
-    if (@filas != other.filas || @columnas != other.columnas)
-      return false
-    end
-    
-    for i in 0...@filas
-      
-      for j in 0...@columnas
-        if (@matriz[i][j] != other[i,j])
-          return false
-        end
+require "./lib/mygem/matriz.rb"
+
+
+module Prct09
+
+  class Matriz_Densa < Matriz
+
+    # Initialize
+    def initialize(filas, columnas)
+      super
+      @pos = Array.new(@filas,0)
+# i = 0
+# while i < @filas
+        @filas.times do |i|
+        @pos[i] = Array.new(@columnas,0)
+# i += 1
       end
     end
-    
-    return true
-  end
-  
-  def [](fila, columna)
-    @matriz[fila][columna]
-  end
 
-  def []=(fila, columna, valor)
-    @matriz[fila][columna] = valor
-  end
-  
-  
+
+ # Get (Devuelve el valor del elemento guardado en i,j)
+    def get(i, j)
+      @pos[i][j]
+    end
+
+ # Set (Modifica el valor del elemento guardado en i,j)
+    def set(i, j, val)
+      @pos[i][j] = val
+    end
+
+ # Porcentaje de numeros nulos
+    def num_nulos
+      total = @filas*@columnas
+      nn = 0
+        @filas.times do |i|
+         @columnas.times do |j|
+# i = 0
+# while(i < @filas)
+# j = 0
+# while(j < @columnas)
+           if(@pos[i][j] != 0)
+             nn += 1
+           end
+# j += 1
+         end
+# i += 1
+       end
+
+      n = total - nn
+
+      n.to_f/total.to_f
+    end
+
+#Definicion del metodo to_s
+    def to_s
+      s = ""
+        @filas.times do |i|
+         @columnas.times do |j|
+
+# i = 0
+# while(i < @filas)
+# j = 0
+# while(j < @columnas)
+          s += "#{@pos[i][j].to_s} "
+# j += 1
+        end
+        puts " "
+# i += 1
+      end
+      s
+    end
+
+
+ # Metodo Suma
+    def +(other)
+      c = Matriz_Densa.new(@filas, @columnas)
+        @filas.times do |i|
+        @columnas.times do |j|
+
+# i = 0
+# while(i < @filas)
+# j = 0
+# while(j < @columnas)
+          c.set(i, j, get(i,j) + other.get(i,j))
+# j += 1
+        end
+# i += 1
+      end
+      c
+    end
+
+# Metodo Resta
+    def -(other)
+      c = Matriz_Densa.new(@filas, @columnas)
+        @filas.times do |i|
+        @columnas.times do |j|
+
+
+# i = 0
+# while(i < @filas)
+# j = 0
+# while(j < @columnas)
+          c.set(i, j, get(i,j) - other.get(i,j))
+# j += 1
+        end
+# i += 1
+      end
+      c
+    end
+
+# Metodo Multiplicacion
+    def *(other)
+        c = Matriz_Densa.new(@filas, other.columnas)
+        @filas.times do |i|
+         other.columnas.times do |j|
+                @columnas.times do |k|
+# i = 0
+# while(i < @filas)
+# j = 0
+# while(j < other.columnas)
+# k = 0
+# while(k < @columnas)
+              c.set(i, j, get(i, k) * other.get(k,j) + c.get(i,j))
+# k += 1
+            end
+# j += 1
+          end
+# i += 1
+        end
+        c
+    end
+
+
+#Metodo Comparacion
+#Busqueda del maximo
+    def max
+        m = get(0,0)
+        @filas.times do |i|
+         @columnas.times do |j|
+# i = 0
+# while(i < @filas)
+# j = 0
+# while(j < @columnas)
+                        if (get(i,j) > m)
+                                m = get(i,j)
+                        end
+# j += 1
+                end
+# i += 1
+        end
+        m
+    end
+
+#Busqueda del minimo
+    def min
+        m = get(0,0)
+        @filas.times do |i|
+         @columnas.times do |j|
+# i = 0
+# while(i < @filas)
+# j = 0
+# while(j < @columnas)
+                        if (get(i,j) < m)
+                                m = get(i,j)
+                        end
+# j += 1
+                end
+# i += 1
+        end
+        m
+    end
+ 
+ end
 end
