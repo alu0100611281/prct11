4010054781fe125026ffa8d766ebda7c0b2ceead
alu0100402001 <alu0100402001@ull.edu.es>
2013-11-29 09:10:35 +0000

    Documentando matriz.rb y matriz_densa.rb con RDOC y generando doc

--- mygem/lib/mygem.rb
+++ mygem/lib/mygem.rb
@@ -1,5 +1,11 @@
-require "mygem/version"
+require "./lib/mygem/version"
+require "./lib/mygem/matriz.rb"
+require "./lib/mygem/matriz_densa.rb"
+require "./lib/mygem/matriz_dispersa.rb"
+
+require "./lib/mygem/fracciones.rb"
+require "./lib/mygem/gcd.rb"
 
 module Mygem
-  # Your code goes here...
+  # Your code goes here..
 end
